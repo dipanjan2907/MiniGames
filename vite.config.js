91e1019b1df4bc808351cde177ad6b34b45d09cc
Dipanjan Sadhukhan <dipanjansadhukhan123@gmail.com>
2026-05-16 00:26:57 +0530

    Revert "."

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react(), tailwindcss()],
-    base: env.VITE_BASE_PATH,
-  };
+export default defineConfig({
+  base: "/MiniGames/",
+  plugins: [tailwindcss(), react()],
 });
