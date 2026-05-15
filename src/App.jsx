336fdc5e9596093bca2d0360be9b29d9b34ea7cc
Dipanjan Sadhukhan <dipanjansadhukhan123@gmail.com>
2026-05-16 00:05:09 +0530

    Added analytics feature for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { Analytics } from "@vercel/analytics/react";
 import LandingPage from "./components/landingPage";
 import MemoryGame1 from "./components/memoryGame1";
 import FlashMemoryGame from "./components/flashMemoryGame";
@@ -10,15 +11,18 @@ function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/allGames" element={<AllGames />} />
-        <Route path="/memoryGame1" element={<MemoryGame1 />} />
-        <Route path="/flash-memory" element={<FlashMemoryGame />} />
-        <Route path="/tic-tac-toe" element={<TicTacToe />} />
-      </Routes>
-    </AnimatePresence>
+    <>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/allGames" element={<AllGames />} />
+          <Route path="/memoryGame1" element={<MemoryGame1 />} />
+          <Route path="/flash-memory" element={<FlashMemoryGame />} />
+          <Route path="/tic-tac-toe" element={<TicTacToe />} />
+        </Routes>
+      </AnimatePresence>
+      <Analytics />
+    </>
   );
 }
 
